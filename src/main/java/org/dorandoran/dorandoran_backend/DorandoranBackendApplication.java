040618775d38999536379ab45dbc4ceb9ef5bfba
apfp77 <apfp77@naver.com>
2024-08-30 11:18:42 +0900

    feat: project

---
+++ src/main/java/org/dorandoran/dorandoran_backend/DorandoranBackendApplication.java
@@ -0,0 +1,13 @@
+package org.dorandoran.dorandoran_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DorandoranBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DorandoranBackendApplication.class, args);
+    }
+
+}
