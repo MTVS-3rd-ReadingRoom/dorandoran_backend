37b42efd1e5ab83a53764a247c37f759400619b5
apfp77 <apfp77@naver.com>
2024-09-06 12:34:35 +0900

    feat: 임시 함수 제거 및 주제 텍스트 API 추가

--- src/main/java/org/dorandoran/dorandoran_backend/voice/AiService.java
+++ src/main/java/org/dorandoran/dorandoran_backend/voice/AiService.java
@@ -1,8 +1,7 @@
 package org.dorandoran.dorandoran_backend.voice;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.dorandoran.dorandoran_backend.common.AiServerUrl;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -11,59 +10,10 @@ import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Service
 public class AiService {
-
-    private static final String AI_SERVER_URL_DISUSSION = "https://sheepdog-bold-bulldog.ngrok-free.app/stt/input_discussion_content";
-    private static final String AI_SERVER_URL_TOPIC = "https://sheepdog-bold-bulldog.ngrok-free.app/discussion_topic/topic_suggest";
-
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    public byte[] sendBinaryDataAndGetResponse(byte[] data) {
-        // Set headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/octet-stream");
-
-        // Prepare request entity
-        HttpEntity<ByteArrayResource> requestEntity = new HttpEntity<>(new ByteArrayResource(data), headers);
-
-        // Send POST request and receive response as byte array
-        try {
-            ResponseEntity<byte[]> response = restTemplate.exchange(
-                    AI_SERVER_URL_DISUSSION,
-                    HttpMethod.POST,
-                    requestEntity,
-                    byte[].class
-            );
-            // 성공 시 응답 처리
-            if (response.getStatusCode().is2xxSuccessful()) {
-                // 정상 응답 처리
-                System.out.println("상태코드: " + response.getStatusCode());
-                return response.getBody();
-
-            } else {
-                // 서버에서 오류 응답 처리
-                System.out.println("서버 오류 응답: " + response.getStatusCode());
-            }
-        } catch (HttpServerErrorException e) {
-            System.err.println("서버에서 500 에러 발생: " + e.getResponseBodyAsString());
-            // 서버에서 발생한 오류 메시지 로깅
-            e.printStackTrace();
-        } catch (Exception e) {
-            System.err.println("예상치 못한 에러 발생: " + e.getMessage());
-            e.printStackTrace();
-        }
-
-        // Return response body
-        return null;
-    }
 
 
     public byte[] sendBinaryDataAndGetResponse(MultipartFile data, String userId, String chatRoom) {
@@ -95,7 +45,7 @@ public class AiService {
         // Send POST request and receive response as byte array
         try {
             ResponseEntity<byte[]> response = restTemplate.exchange(
-                    AI_SERVER_URL_DISUSSION,
+                    AiServerUrl.DISCUSSION,
                     HttpMethod.POST,
                     requestEntity,
                     byte[].class
@@ -110,14 +60,13 @@ public class AiService {
                 System.out.println("서버 오류 응답: " + response.getStatusCode());
             }
         } catch (HttpServerErrorException e) {
-            System.err.println("서버에서 500 에러 발생: " + e.getResponseBodyAsString());
+            System.err.println("AI 요청 에러: " + e.getResponseBodyAsString());
             e.printStackTrace();
         } catch (Exception e) {
             System.err.println("예상치 못한 에러 발생: " + e.getMessage());
             e.printStackTrace();
         }
 
-        // Return response body
         return null;
     }
 
@@ -132,17 +81,55 @@ public class AiService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("chat_room_id", chatRoomId);
 
+        System.out.println(chatRoomId);
         // HttpEntity 생성
         HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(body, headers);
 
         // 요청 보내기
-        ResponseEntity<byte[]> response = restTemplate.exchange(AI_SERVER_URL_TOPIC, HttpMethod.POST, requestEntity, byte[].class);
+        try{
+            ResponseEntity<byte[]> response = restTemplate.exchange(AiServerUrl.TOPIC, HttpMethod.POST, requestEntity, byte[].class);
+            // 응답 상태 확인
+            if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
+                byte[] fileContent = response.getBody();
 
-        // 응답 상태 확인
-        if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
-            byte[] fileContent = response.getBody();
+                return fileContent;
+            }
+        } catch (HttpServerErrorException e) {
+            System.err.println("AI 요청 에러: " + e.getResponseBodyAsString());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("예상치 못한 에러 발생: " + e.getMessage());
+            e.printStackTrace();
+        }
 
-            return fileContent;
+        throw new RuntimeException("Failed to fetch audio data");
+    }
+
+    public String sendChatRoomResponseText(String chatRoomId) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        // 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // 바디에 들어갈 데이터 설정
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("chat_room_id", chatRoomId);
+
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(body, headers);
+
+        try{
+            ResponseEntity<String> response = restTemplate.exchange(AiServerUrl.TOPIC_TEXT, HttpMethod.POST, requestEntity, String.class);
+            // 응답 상태 확인
+            if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
+                return response.getBody();
+            }
+        } catch (HttpServerErrorException e) {
+            System.err.println("AI 요청 에러: " + e.getResponseBodyAsString());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("예상치 못한 에러 발생: " + e.getMessage());
+            e.printStackTrace();
         }
 
         throw new RuntimeException("Failed to fetch audio data");
--- src/main/java/org/dorandoran/dorandoran_backend/voice/ApiController.java
+++ src/main/java/org/dorandoran/dorandoran_backend/voice/ApiController.java
@@ -1,6 +1,8 @@
 package org.dorandoran.dorandoran_backend.voice;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,39 +14,45 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "AI 요청 API")
 public class ApiController {
 
     @Autowired
     private AiService aiService;
 
     @PostMapping("/send-data")
-    // public ResponseEntity<byte[]> receiveAndForwardData(@RequestBody byte[] data) {
-    public ResponseEntity<byte[]> receiveAndForwardData(
-            @RequestParam("file") MultipartFile data,
-            @RequestParam("user_id") String userId,
-            @RequestParam("chat_room_id") String chat_room
-    ){
+    @Operation(summary = "발언 학습")
+    public ResponseEntity<byte[]> receiveAndForwardData(@RequestParam("file") MultipartFile data, @RequestParam("user_id") String userId, @RequestParam("chat_room_id") String chat_room) {
         // Forward data to AI server and get response
         byte[] responseData = aiService.sendBinaryDataAndGetResponse(data, userId, chat_room);
 
-        // Set headers
+        // AI에서 테스트용 음성 파일 반환
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "audio/mpeg"); // or the appropriate MIME type for your audio
+        headers.set("Content-Type", "audio/mpeg");
 
-        // Return response to the client
         return new ResponseEntity<>(responseData, headers, HttpStatus.OK);
     }
 
     // 채팅방을 입력받아 서버로 application/x-www-form-urlencoded 형식으로 전송
     @PostMapping("/topic_suggest")
-    public ResponseEntity<?> sendChatRoom(
-            @RequestParam("chat_room_id") String chat_room_id
-    ) throws IOException {
+    @Operation(summary = "주제 추천(음성)")
+    public ResponseEntity<?> sendChatRoom(@RequestParam("chat_room_id") String chat_room_id) {
         // Forward data to AI server and get response
         byte[] responseData = aiService.sendChatRoom(chat_room_id);
         // Set headers
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "audio/wav"); // or the appropriate MIME type for your audio
+
+        return new ResponseEntity<>(responseData, headers, HttpStatus.OK);
+    }
+    @PostMapping("/topic_suggest-text")
+    @Operation(summary = "주제 추천(text)")
+    public ResponseEntity<?> sendChatRoomResponseText(@RequestParam("chat_room_id") String chat_room_id) {
+
+        String responseData = aiService.sendChatRoomResponseText(chat_room_id);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
 
         return new ResponseEntity<>(responseData, headers, HttpStatus.OK);
     }
