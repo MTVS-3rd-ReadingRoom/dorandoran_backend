aceb1b435c67bdd730f1dc1251d4581993840b7a
apfp77 <apfp77@naver.com>
2024-09-06 12:33:47 +0900

    feat: 상수 추가

---
+++ src/main/java/org/dorandoran/dorandoran_backend/common/AiServerUrl.java
@@ -0,0 +1,8 @@
+package org.dorandoran.dorandoran_backend.common;
+
+public class AiServerUrl {
+    public static final String SUMMARY = "https://sheepdog-bold-bulldog.ngrok-free.app/discussion_summary/discussion_summary";
+    public static final String DISCUSSION = "https://sheepdog-bold-bulldog.ngrok-free.app/stt/input_discussion_content";
+    public static final String TOPIC = "https://sheepdog-bold-bulldog.ngrok-free.app/discussion_topic/topic_suggest";
+    public static final String TOPIC_TEXT = "https://sheepdog-bold-bulldog.ngrok-free.app/discussion_topic/topic_suggest_text";
+}
